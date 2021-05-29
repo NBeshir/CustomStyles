7556eb4bd670ba7e7c0f071cef7362584e66e375
Nejmiya Beshir <tnejmi@gmail.com>
2021-05-29 16:37:38 -0700

    campsite first commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 $(function () {
-    $(".carousel").carousel({ interval: 2000 });
-    $("#carouselButton").click(function () {
-      if ($("#carouselButton").children("i").hasClass("fa-pause")) {
-        $(".carousel").carousel("pause");
-        $("#carouselButton").children("i").removeClass("fa-pause");
-        $("#carouselButton").children("i").addClass("fa-play");
-      }
-
-      else {
-       
-        $(".carousel").carousel("cycle"); //jquery starts the cycle again
-        $("#carouselButton").children("i").removeClass("fa-play");
-        $("#carouselButton").children("i").addClass("fa-pause");
-      }
-    
-    });
+  $(".carousel").carousel({ interval: 2000 });
+  $("#carouselButton").click(function () {
+    if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+      $(".carousel").carousel("pause");
+      $("#carouselButton").children("i").removeClass("fa-pause");
+      $("#carouselButton").children("i").addClass("fa-play");
+    } else {
+      $(".carousel").carousel("cycle"); //jquery starts the cycle again
+      $("#carouselButton").children("i").removeClass("fa-play");
+      $("#carouselButton").children("i").addClass("fa-pause");
+    }
   });
+  //const reserveButton = document.getElementById("reserveButton");
+  //reserveButton.addEventListener("click", () => {
+  $("#reserveButton").click(function () {
+    $("#reserveModal").modal();
+  });
+
+  //const loginButton = document.getElementById("loginButton");
+  // loginButton.addEventListener("click", () => {
+  $("#loginButton ").click(function () {
+    $("#loginModal").modal();
+  });
+});
